4f8c503612e624bbb6716a6d15c0f6574d95e998
JPivista <jpivistasolutions@gmail.com>
2024-09-16 12:25:01 +0530

    cdn is updated on next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/api/:path*', // Adjust source as needed
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://ivista-digital-bucket.blr1.cdn.digitaloceanspaces.com/',
+                    },
+                ],
+            },
+        ];
+    },
+}
 
 module.exports = nextConfig
