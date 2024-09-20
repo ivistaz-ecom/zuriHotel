702d35370acc707e4a0dd0df0ac516b67023cceb
sibiraj-002 <shansibi2002@gmail.com>
2024-09-20 11:34:34 +0530

    meta describtion and title added in customer reviews page

--- components/CustomerReviewComponents/index.jsx
+++ components/CustomerReviewComponents/index.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 import Header from '../Shared/Header'
 import Footer from '../Shared/Footer'
 import CustomerReview from './CustomerReview'
+import NextSeo from '../SeoComponents/Seo'
+
+const title = "Guest Reviews: The Zuri Experience";
+const description = "From luxurious accommodations to exceptional dining, The Zuri offers it all. Explore our guest reviews and discover why we're a top choice for travelers.";
+const path = "https://www.thezurihotels.com/customer-review";
+const metaImage = "/zuri_logo_social_media.png";
+
 
 const index = () => {
     return (
         <>
+         <NextSeo title={title} description={description} path={path} metaImage={metaImage} />
             <Header />
             <CustomerReview />
             <Footer />
