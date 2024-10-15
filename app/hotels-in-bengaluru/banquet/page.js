134bd1cd9516bb5d5ddc9ed6d6b51ec00a7a79f3
Sibiraj Kandhasawy <sibirajkandhasawy@Mac.localdomain>
2024-10-15 16:16:55 +0530

    image location changed

--- app/hotels-in-bengaluru/banquet/page.js
+++ app/hotels-in-bengaluru/banquet/page.js
@@ -6,20 +6,15 @@ const BanquetImage = () => {
         <div className="container-fluid p-0 m-0">
             <div className="d-flex justify-content-center align-items-center">
                 <img 
-                    className="w-full h-full" 
+                    className="w-100 h-100" 
                     width={900} 
                     height={600} 
                     src="/banquetBengaluru001.jpg" 
                     alt="Banquet Banner"
-                   
+                    style={{width:'100% !important',height:'100% !important'}}
                 />
              
                
-
-
-
-
-
             </div>
         </div>
     )
