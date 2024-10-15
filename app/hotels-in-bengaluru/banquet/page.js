9a0080acd99dc70f304f280e09838aa6d5e21b82
Sibiraj Kandhasawy <sibirajkandhasawy@Mac.localdomain>
2024-10-15 16:16:12 +0530

    image location changed

--- app/hotels-in-bengaluru/banquet/page.js
+++ app/hotels-in-bengaluru/banquet/page.js
@@ -1,50 +1,19 @@
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 const BanquetImage = () => {
     return (
         <div className="container-fluid p-0 m-0">
             <div className="d-flex justify-content-center align-items-center">
-                <Image 
-                    className="w-100 h-100" 
+                <img 
+                    className="w-full h-full" 
                     width={900} 
                     height={600} 
                     src="/banquetBengaluru001.jpg" 
                     alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
+                   
                 />
-               <Image
-                    className="w-100 h-100" 
-                    width={900} 
-                    height={600} 
-                    src="/banquetBengaluru002.jpg" 
-                    alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
-                />
-                 <Image
-                    className="w-100 h-100" 
-                    width={900} 
-                    height={600} 
-                    src="/banquet_bengaluru_003.jpg" 
-                    alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
-                />
-                 <Image
-                    className="w-100 h-100" 
-                    width={900} 
-                    height={600} 
-                    src="/banquet_bengaluru_004.jpg" 
-                    alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
-                />
-                 <Image
-                    className="w-100 h-100" 
-                    width={900} 
-                    height={600} 
-                    src="/banquet_bengaluru_005.jpg" 
-                    alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
-                />
+             
                
 
 
