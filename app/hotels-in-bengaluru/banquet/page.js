fb45178cc303289cdcb5e26221894923a7568077
Sibiraj Kandhasawy <sibirajkandhasawy@Mac.localdomain>
2024-10-15 16:20:26 +0530

    image location changed

--- app/hotels-in-bengaluru/banquet/page.js
+++ app/hotels-in-bengaluru/banquet/page.js
@@ -1,18 +1,32 @@
 import React from 'react'
 // import Image from 'next/image'
 
+const ImageLists = [
+{imgUrl:'/banquetBengaluru001.jpg'},
+{imgUrl:'/banquetBengaluru001.jpg'},
+
+]
+
+
+
 const BanquetImage = () => {
     return (
         <div className="container-fluid p-0 m-0">
             <div className="d-flex justify-content-center align-items-center">
-                <img 
-                    className="w-100 h-100" 
-                    width={900} 
-                    height={600} 
-                    src="/banquetBengaluru001.jpg" 
-                    alt="Banquet Banner"
-                    style={{width:'100% !important',height:'100% !important'}}
-                />
+                {ImageLists.map((items,index)=>(
+
+<img 
+className="w-100 h-100" 
+width={900} 
+height={600} 
+src={items.imgUrl} 
+alt="Banquet Banner"
+style={{width:'100% !important',height:'100% !important'}}
+key={index}
+/>
+
+                ))}
+               
              
                
             </div>
