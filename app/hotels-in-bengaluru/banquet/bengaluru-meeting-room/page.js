013b0b73ff5b632891e37c981d9e4eb751311fda
Sibiraj Kandhasawy <sibirajkandhasawy@Mac.localdomain>
2024-10-15 16:12:13 +0530

    image location changed

--- app/hotels-in-bengaluru/banquet/bengaluru-meeting-room/page.js
+++ app/hotels-in-bengaluru/banquet/bengaluru-meeting-room/page.js
@@ -6,7 +6,7 @@ const BengaluruMeetingRoom = () => {
         <div className="container-fluid p-0 m-0">
             <div className="d-flex justify-content-center align-items-center">
                  <Image 
-                    className="w-100 lg:h-100 h-screen" 
+                    className="w-100 lg:h-full h-screen" 
                     width={900} 
                     height={600} 
                     src="/bengaluru/banquet/bengaluru-img.jpeg" 
