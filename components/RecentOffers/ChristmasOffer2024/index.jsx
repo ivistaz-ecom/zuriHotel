98f202eae4306aca04b6a3dfe55aaa723a56015f
JPivista <jpivistasolutions@gmail.com>
2024-12-13 16:26:07 +0530

    christmas and new year page is updated

--- components/RecentOffers/ChristmasOffer2024/index.jsx
+++ components/RecentOffers/ChristmasOffer2024/index.jsx
@@ -3,10 +3,20 @@ import ChristmasBanner from './ChristmasBanner'
 import Header from '../../../components/Shared/Header'
 import Footer from '../../../components/Shared/Footer'
 import ChristmaContactForm from '../../Forms/ChristmasNewYear2024Form'
+import NextSeo from '@/components/SeoComponents/Seo'
+
+
+const title = "Christmas and New Year at Zuri";
+const description = "Celebrate your holidays with Zuri. Book your stay and make it a memorable holiday season.";
+const path = "https://www.thezurihotels.com/";
+const metaImage = "/zuri_logo_social_media.png";
 
 const index = () => {
     return (
         <>
+
+            <NextSeo title={title} description={description} path={path} metaImage={metaImage} />
+
             <Header />
             <ChristmasBanner />
             <ChristmaContactForm />
