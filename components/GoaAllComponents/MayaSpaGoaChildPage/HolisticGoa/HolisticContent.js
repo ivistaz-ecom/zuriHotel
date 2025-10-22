e0105429fba9708abd47397956342bed030394ee
sibiraj-002 <shansibi2002@gmail.com>
2025-10-22 11:31:33 +0530

    Price removed form offers page.

--- components/GoaAllComponents/MayaSpaGoaChildPage/HolisticGoa/HolisticContent.js
+++ components/GoaAllComponents/MayaSpaGoaChildPage/HolisticGoa/HolisticContent.js
@@ -22,11 +22,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 60 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             4289 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         Aromatherapy is the ancient science of healing, relaxing and energizing through Nervous System by the use of plants and their parts. The massage is with a blend of essential oils and carrier oil which penetrate the bloodstream and soothe the central nervous system. The massage activates the body’s nerve endings and stimulates its blood flow.
                     </p>
@@ -39,11 +39,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 60 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             4289 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         Traditional Classical European massage with medium pressure which relieves the muscular tension to stimulate soft tissues resulting in improving the lymphatic flow, relieves stress.
                     </p>
@@ -56,11 +56,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 60 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             4730 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         A deep tissue massage using a combination of gentle stretches, acupressure to stimulate the flow of blood, oxygen and chi’ (energy) around your body and bring relaxation and wellness.
                     </p>
@@ -74,11 +74,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 60 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             5000 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         A deep tissue pressure massage done with specific anticellulite oils which help to loosen and break the cellulite along with relaxation and improving the blood circulation.
                     </p>
@@ -91,11 +91,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 60 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             4289 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         This is the medium pressure massage in which the cream is used instead of oils.It helps to exfoliate the skin,helps in lymph drainage and makes the skin supple and beautiful.
                     </p>
@@ -108,11 +108,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 30 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             2500 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         A relaxing back, neck and shoulder massage with medicated oils to release the knots and stress in the muscles.
                     </p>
@@ -125,11 +125,11 @@ const HolisticContent = () => {
                     <p>
                         Duration: 30 minutes
                     </p>
-                    <p className='text-black'>
+                    {/* <p className='text-black'>
                         <b>
                             2500 INR
                         </b>
-                    </p>
+                    </p> */}
                     <p className='d-none d-md-block'>
                         A massage on the reflex areas of the foot which helps to flash out toxins and inflammations through lymph drainage and give the whole body a total relaxing state.
                     </p>
